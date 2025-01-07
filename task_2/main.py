6ce6bb5c1d52141d7c7729fe49269c67641915dd
Ксенія Серховець <Kseniia.Serkhovets@infiz.khpi.edu.ua>
2025-01-07 19:29:44 +0200

    2.3

--- task_2/main.py
+++ task_2/main.py
@@ -1,33 +1,25 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    min_max = []
+    numbers_list = []
 
-    for i in range(min, max):
-        min_max.append(i)
-
-    numbers_list = random.sample(min_max, quantity)
-    numbers_list.sort()
+    if min < 1 or min > 999:
+        print("Min is invalid")
+    elif max <= min or max > 1000:
+        print("Max is invalid")
+    elif quantity > max - min:
+        print("Quantity is invalid")
+    else:
+        min_max = list(range(min, max))
+        numbers_list = random.sample(min_max, quantity)
+        numbers_list.sort()
     
     return numbers_list
 
-def check_input(min, max, quantity):
-    if min < 1 or min > 999:
-        print("Min is invalid")
-        return False
-    elif max <= min or max > 1000:
-        print("Max is invalid")
-        return False
-    elif quantity > max - min:
-        print("Quantity is invalid")
-        return False
-    return True
 
 min = int(input("Enter min: "))
 max = int(input("Enter max: "))
 quantity = int(input("Enter quantity: "))
 
-
-if check_input(min, max, quantity):
-    lottery_numbers = get_numbers_ticket(min, max, quantity)
-    print("Ваші лотерейні числа:", lottery_numbers)
+lottery_numbers = get_numbers_ticket(min, max, quantity)
+print("Ваші лотерейні числа:", lottery_numbers)
