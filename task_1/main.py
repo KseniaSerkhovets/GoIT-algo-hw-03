5d4d58036b1eb271be5e7b72b290405eb05c9a41
Ксенія Серховець <Kseniia.Serkhovets@infiz.khpi.edu.ua>
2025-01-07 19:19:42 +0200

    1.2

--- task_1/main.py
+++ task_1/main.py
@@ -2,14 +2,14 @@ from datetime import datetime, date
 import re
 
 def get_days_from_today(date):
-    if re.match(r"(\d{4})-(\d{2})-(\d{2})", date):
+    try:
         now_date = datetime.now()
         past_date = datetime.strptime(date, "%Y-%m-%d")
         differnce = (now_date-past_date).days
-        print(differnce)
-    else:
+        return differnce
+    except:
         print("Wrong format of date")
-    return 0
+
 
 date = input("Print the date, days from wich you whant to count (РРРР-ММ-ДД): ")
-get_days_from_today(date)
+print(get_days_from_today(date))
